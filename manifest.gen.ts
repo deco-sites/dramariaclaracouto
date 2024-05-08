37389fd6144bc2f135905bf24a300769ceb22e2b
Rodrigo Araujo <rodrigo.eritel@gmail.com>
2024-05-07 21:25:20 -0300

    fix: upgrade deno

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,53 +2,53 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$$$$$$$0 from "./apps/decohub.ts";
-import * as $$$$$$$$$$$$1 from "./apps/site.ts";
-import * as $$$$$$$0 from "./sections/AgendaSuaConsulta.tsx";
-import * as $$$$$$$1 from "./sections/Atendimento.tsx";
-import * as $$$$$$$2 from "./sections/BannerPrincipal.tsx";
-import * as $$$$$$$3 from "./sections/BannerPrincipal/BannerPrincipal.tsx";
-import * as $$$$$$$4 from "./sections/Benefits.tsx";
-import * as $$$$$$$5 from "./sections/BlogPost.tsx";
-import * as $$$$$$$6 from "./sections/BlogPosts.tsx";
-import * as $$$$$$$7 from "./sections/Faq.tsx";
-import * as $$$$$$$8 from "./sections/Footer.tsx";
-import * as $$$$$$$9 from "./sections/Header.tsx";
-import * as $$$$$$$10 from "./sections/Hero.tsx";
-import * as $$$$$$$11 from "./sections/ImageWithParagraph.tsx";
-import * as $$$$$$$12 from "./sections/Location.tsx";
-import * as $$$$$$$13 from "./sections/Logos.tsx";
-import * as $$$$$$$14 from "./sections/MainPost.tsx";
-import * as $$$$$$$15 from "./sections/OQueEncontrar.tsx";
-import * as $$$$$$$16 from "./sections/SobreMim.tsx";
-import * as $$$$$$$17 from "./sections/Testimonials.tsx";
-import * as $$$$$$$18 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$1 from "./apps/site.ts";
+import * as $$$$$$0 from "./sections/AgendaSuaConsulta.tsx";
+import * as $$$$$$1 from "./sections/Atendimento.tsx";
+import * as $$$$$$2 from "./sections/BannerPrincipal.tsx";
+import * as $$$$$$3 from "./sections/BannerPrincipal/BannerPrincipal.tsx";
+import * as $$$$$$4 from "./sections/Benefits.tsx";
+import * as $$$$$$5 from "./sections/BlogPost.tsx";
+import * as $$$$$$6 from "./sections/BlogPosts.tsx";
+import * as $$$$$$7 from "./sections/Faq.tsx";
+import * as $$$$$$8 from "./sections/Footer.tsx";
+import * as $$$$$$9 from "./sections/Header.tsx";
+import * as $$$$$$10 from "./sections/Hero.tsx";
+import * as $$$$$$11 from "./sections/ImageWithParagraph.tsx";
+import * as $$$$$$12 from "./sections/Location.tsx";
+import * as $$$$$$13 from "./sections/Logos.tsx";
+import * as $$$$$$14 from "./sections/MainPost.tsx";
+import * as $$$$$$15 from "./sections/OQueEncontrar.tsx";
+import * as $$$$$$16 from "./sections/SobreMim.tsx";
+import * as $$$$$$17 from "./sections/Testimonials.tsx";
+import * as $$$$$$18 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
-    "site/sections/AgendaSuaConsulta.tsx": $$$$$$$0,
-    "site/sections/Atendimento.tsx": $$$$$$$1,
-    "site/sections/BannerPrincipal.tsx": $$$$$$$2,
-    "site/sections/BannerPrincipal/BannerPrincipal.tsx": $$$$$$$3,
-    "site/sections/Benefits.tsx": $$$$$$$4,
-    "site/sections/BlogPost.tsx": $$$$$$$5,
-    "site/sections/BlogPosts.tsx": $$$$$$$6,
-    "site/sections/Faq.tsx": $$$$$$$7,
-    "site/sections/Footer.tsx": $$$$$$$8,
-    "site/sections/Header.tsx": $$$$$$$9,
-    "site/sections/Hero.tsx": $$$$$$$10,
-    "site/sections/ImageWithParagraph.tsx": $$$$$$$11,
-    "site/sections/Location.tsx": $$$$$$$12,
-    "site/sections/Logos.tsx": $$$$$$$13,
-    "site/sections/MainPost.tsx": $$$$$$$14,
-    "site/sections/OQueEncontrar.tsx": $$$$$$$15,
-    "site/sections/SobreMim.tsx": $$$$$$$16,
-    "site/sections/Testimonials.tsx": $$$$$$$17,
-    "site/sections/Theme/Theme.tsx": $$$$$$$18,
+    "site/sections/AgendaSuaConsulta.tsx": $$$$$$0,
+    "site/sections/Atendimento.tsx": $$$$$$1,
+    "site/sections/BannerPrincipal.tsx": $$$$$$2,
+    "site/sections/BannerPrincipal/BannerPrincipal.tsx": $$$$$$3,
+    "site/sections/Benefits.tsx": $$$$$$4,
+    "site/sections/BlogPost.tsx": $$$$$$5,
+    "site/sections/BlogPosts.tsx": $$$$$$6,
+    "site/sections/Faq.tsx": $$$$$$7,
+    "site/sections/Footer.tsx": $$$$$$8,
+    "site/sections/Header.tsx": $$$$$$9,
+    "site/sections/Hero.tsx": $$$$$$10,
+    "site/sections/ImageWithParagraph.tsx": $$$$$$11,
+    "site/sections/Location.tsx": $$$$$$12,
+    "site/sections/Logos.tsx": $$$$$$13,
+    "site/sections/MainPost.tsx": $$$$$$14,
+    "site/sections/OQueEncontrar.tsx": $$$$$$15,
+    "site/sections/SobreMim.tsx": $$$$$$16,
+    "site/sections/Testimonials.tsx": $$$$$$17,
+    "site/sections/Theme/Theme.tsx": $$$$$$18,
   },
   "apps": {
-    "site/apps/decohub.ts": $$$$$$$$$$$$0,
-    "site/apps/site.ts": $$$$$$$$$$$$1,
+    "site/apps/decohub.ts": $$$$$$$$$$$0,
+    "site/apps/site.ts": $$$$$$$$$$$1,
   },
   "name": "site",
   "baseUrl": import.meta.url,
