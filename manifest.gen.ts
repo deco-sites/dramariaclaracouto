796df828d1610dc72572104a8043441ecf1f6079
Rodrigo Araujo <rodrigo.eritel@gmail.com>
2024-07-01 13:59:05 -0300

    feat: add new component

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -11,18 +11,19 @@ import * as $$$$$$3 from "./sections/BannerPrincipal/BannerPrincipal.tsx";
 import * as $$$$$$4 from "./sections/Benefits.tsx";
 import * as $$$$$$5 from "./sections/BlogPost.tsx";
 import * as $$$$$$6 from "./sections/BlogPosts.tsx";
-import * as $$$$$$7 from "./sections/Faq.tsx";
-import * as $$$$$$8 from "./sections/Footer.tsx";
-import * as $$$$$$9 from "./sections/Header.tsx";
-import * as $$$$$$10 from "./sections/Hero.tsx";
-import * as $$$$$$11 from "./sections/ImageWithParagraph.tsx";
-import * as $$$$$$12 from "./sections/Location.tsx";
-import * as $$$$$$13 from "./sections/Logos.tsx";
-import * as $$$$$$14 from "./sections/MainPost.tsx";
-import * as $$$$$$15 from "./sections/OQueEncontrar.tsx";
-import * as $$$$$$16 from "./sections/SobreMim.tsx";
-import * as $$$$$$17 from "./sections/Testimonials.tsx";
-import * as $$$$$$18 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$7 from "./sections/CapturePage.tsx";
+import * as $$$$$$8 from "./sections/Faq.tsx";
+import * as $$$$$$9 from "./sections/Footer.tsx";
+import * as $$$$$$10 from "./sections/Header.tsx";
+import * as $$$$$$11 from "./sections/Hero.tsx";
+import * as $$$$$$12 from "./sections/ImageWithParagraph.tsx";
+import * as $$$$$$13 from "./sections/Location.tsx";
+import * as $$$$$$14 from "./sections/Logos.tsx";
+import * as $$$$$$15 from "./sections/MainPost.tsx";
+import * as $$$$$$16 from "./sections/OQueEncontrar.tsx";
+import * as $$$$$$17 from "./sections/SobreMim.tsx";
+import * as $$$$$$18 from "./sections/Testimonials.tsx";
+import * as $$$$$$19 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
@@ -33,18 +34,19 @@ const manifest = {
     "site/sections/Benefits.tsx": $$$$$$4,
     "site/sections/BlogPost.tsx": $$$$$$5,
     "site/sections/BlogPosts.tsx": $$$$$$6,
-    "site/sections/Faq.tsx": $$$$$$7,
-    "site/sections/Footer.tsx": $$$$$$8,
-    "site/sections/Header.tsx": $$$$$$9,
-    "site/sections/Hero.tsx": $$$$$$10,
-    "site/sections/ImageWithParagraph.tsx": $$$$$$11,
-    "site/sections/Location.tsx": $$$$$$12,
-    "site/sections/Logos.tsx": $$$$$$13,
-    "site/sections/MainPost.tsx": $$$$$$14,
-    "site/sections/OQueEncontrar.tsx": $$$$$$15,
-    "site/sections/SobreMim.tsx": $$$$$$16,
-    "site/sections/Testimonials.tsx": $$$$$$17,
-    "site/sections/Theme/Theme.tsx": $$$$$$18,
+    "site/sections/CapturePage.tsx": $$$$$$7,
+    "site/sections/Faq.tsx": $$$$$$8,
+    "site/sections/Footer.tsx": $$$$$$9,
+    "site/sections/Header.tsx": $$$$$$10,
+    "site/sections/Hero.tsx": $$$$$$11,
+    "site/sections/ImageWithParagraph.tsx": $$$$$$12,
+    "site/sections/Location.tsx": $$$$$$13,
+    "site/sections/Logos.tsx": $$$$$$14,
+    "site/sections/MainPost.tsx": $$$$$$15,
+    "site/sections/OQueEncontrar.tsx": $$$$$$16,
+    "site/sections/SobreMim.tsx": $$$$$$17,
+    "site/sections/Testimonials.tsx": $$$$$$18,
+    "site/sections/Theme/Theme.tsx": $$$$$$19,
   },
   "apps": {
     "site/apps/decohub.ts": $$$$$$$$$$$0,
---
+++ sections/CapturePage.tsx
@@ -0,0 +1,5 @@
+export default function CapturePage() {
+    return (
+        <div></div>
+    )
+}
